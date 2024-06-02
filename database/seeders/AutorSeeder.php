5d0e81c48cee9f99c22b8be18b7d3fd79e773554
Rafael Paulino <rafaclasses@gmail.com>
2024-06-02 20:40:38 -0300

    migrations e seeders criadas

--- database/seeders/AutorSeeder.php
+++ database/seeders/AutorSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 use App\Models\Autor;
 
 class AutorSeeder extends Seeder
@@ -14,12 +13,6 @@ class AutorSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
-
-        for ($i = 0; $i < 10; $i++) {
-            Autor::create([
-                'nome' => $faker->firstName(40)
-            ]);
-        }
+        Autor::factory()->count(3)->create();
     }
 }
--- database/seeders/LivroSeeder.php
+++ database/seeders/LivroSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Livro;
+use App\Models\Autor;
+use App\Models\Assunto;
 
 class LivroSeeder extends Seeder
 {
@@ -14,7 +16,9 @@ class LivroSeeder extends Seeder
     public function run(): void
     {
         Livro::factory()
-            ->count(20)
+            ->has(Autor::factory()->count(2), 'autores')
+            ->has(Assunto::factory()->count(2), 'assuntos')
+            ->count(15)
             ->create();
     }
 }
